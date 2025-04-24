433d4c66eb1771dcad84af7a794d14cd16e18f4f
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2025-04-24 11:28:22 +0300

    Make project files

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
 # Snake
+
+![Snake Website](./images/Snake.png)
+
+---
+
+## Game
+
+A snake and a food item are displayed on the board , the snake moves automatically and the player is able to change its direction in order to eat the food item, once the food item is eaten it reappears somewhere else on the board and increases the snake's length and increases the players score, once the snake hits itself or the wall the player loses and able to restart again.
+
+---
+
+## Functionality
+
+- Choose difficulty between easy medium and difficult that changes the snake speed.
+- Change snake direction by clicking arrows.
+- Increase snake length once eaten food.
+- Increase the score once the snake eats food.
+- Relocate the food on the board once snake eats food.
+- Restart game if lost or restart button clicked.
+- Display lost one the player looses.
+
+---
+
+## Credits
---
+++ script.js
@@ -0,0 +1,30 @@
+// Global Variables
+
+// Select the board
+// Make cell divs
+// Append div cells to the board
+// Select the difficulty buttons
+// Select the restart button
+// Select the score display
+// Make snake object with attributes (length: number, speed: number, cells: array) and methods eat and move
+// Make Food object with method changeLocation
+// Make string variable for difficulty
+// Make variable for score
+
+// Functions
+
+// Write a function that changes the difficulty variable and the snake speed
+// Write a function that starts game
+// Write a function that updates score
+// write a function that displays lost
+// Write function that resets the length of the snake and score and restarts the game
+
+// Event Listeners
+
+// Enable changing snake direction to by arrow keyboard clicks
+// Display the snake on the board by taking the cells in the snake object and changing the colors of the corresponding cells on the board cells
+// Add restart function event listeners in to the restart button
+// Add event listener to the difficulty button
+// Add event listener that displays lost win snake hits the wall or itself
+
+console.log('Hello World!')
