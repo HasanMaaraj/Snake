6039beffdc211434672337ff06e340b2079fcb81
HasanMaaraj <123746697+HasanMaaraj@users.noreply.github.com>
2025-04-27 09:55:10 +0300

    Make snake object

--- script.js
+++ script.js
@@ -8,7 +8,42 @@ console.log(boardContainer)
 // Select the difficulty buttons
 // Select the restart button
 // Select the score display
-// Make snake object with attributes (length: number, speed: number, cells: array) and methods eat and move
+// Make snake object with attributes (locations: array, direction: string, hasEaten: boolean) and method move
+const snake = {
+  locations: [
+    [22, 29],
+    [22, 30],
+    [22, 31],
+    [22, 32],
+    [22, 33],
+    [22, 34],
+    [22, 35],
+    [22, 36],
+    [22, 37],
+    [22, 38],
+  ],
+  direction: 'left',
+  hasEaten: false,
+  move() {
+    let newLocation
+    switch (this.direction) {
+      case 'left':
+        newLocation = [this.locations[0][0], this.locations[0][1]-1]
+        break
+      case 'up':
+        newLocation = [this.locations[0][0]-1, this.locations[0][1]]
+        break
+      case 'right':
+        newLocation = [this.locations[0][0], this.locations[0][1]+1]
+        break
+      case 'down':
+        newLocation = [this.locations[0][0]+1, this.locations[0][1]]
+        break
+    }
+    this.locations.unshift(newLocation)
+    this.locations.pop()
+  }
+}
 // Make Food object with method changeLocation
 // Make string variable for difficulty
 // Make variable for score
